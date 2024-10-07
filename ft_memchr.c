120ec8ae435d37ea3c7f61763aba10db8b06bbb7
Maria Conde Sanchez <mconde-s@c1r16s4.42madrid.com>
2024-10-07 19:59:47 +0200

    :)

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,15 @@
 /*   By: mconde-s <mconde-s>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 20:47:39 by mconde-s          #+#    #+#             */
-/*   Updated: 2024/10/06 20:58:07 by mconde-s         ###   ########.fr       */
+/*   Updated: 2024/10/07 17:24:42 by mconde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *memchr(const void *s, int c, size_t n)
+//con los corchetes lo desplazas y accedes y con esto coge s1 y lo mueve i caracteres.
+
+void *ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char *s1;
 	size_t i;
@@ -20,11 +22,20 @@ void *memchr(const void *s, int c, size_t n)
 	s1 = (unsigned char *)s;
 	i = 0;
 
-	while (n > i && s1[i] && s1[i] != (char)c)
+	c = (unsigned char)c;
+	while (i < n)
+	{
+		if (s1[i] == c)
+			return (s1 + i); //Te devuelve la posición en la que se encuentra.
 		i++;
-	return(*s1);
+	}
+	return (NULL);
 }
 int main()
 {
-	char a[]
+	char a[] = "HelloThere";
+	char *b;
+	b = ft_memchr(a, 'e', 4);
+	printf("%c", (*b));
+	return(0);
 }
---
+++ ft_strnstr.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mconde-s <mconde-s>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 18:16:27 by mconde-s          #+#    #+#             */
+/*   Updated: 2024/10/07 19:56:31 by mconde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//Con la funcion strnstr puedes buscar una cadena mas pequeña dentro de otra cadena mas grande pasandole un total de n caracteres, 
+//eso te devuelve un puntero al inicio de la cadena y lo puedes usar para a partir de ahi sustituir n caracteres de la cadena principal.
+//VALORES DE RETORNO
+//Si little es una cadena vacía, se devuelve big; 
+//si little no aparece en ninguna parte de big, se devuelve NULL; de lo contrario, se devuelve un puntero al primer carácter de la primera ocurrencia de little.
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t i;
+	size_t j;
+	
+	i = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] == little[j] && i + j < len && little[j] != 0)
+			j++;
+		if (little[j] == '\0')
+			return((char *)big + i); //es big + i porque despues de entrar en todo el while,, i se encontra
+			
+		i++;
+	}
+	return (0);
+}
+int main()
+{
+	char a[] = "HelloThere";
+	char b[] = "There";
+	printf("%s", ft_strnstr(a, b, 11));
+	return(0);
+}
+/*while (i < len && big[i])
+	{
+		while (big[i] != little[i] && big[i] != little[i + 1])
+		{
+			i++;
+			return(little[i] + i);
+		}
+		
+			
+		if (big[i] == little[i] && little[i] == '\0')
+		{
+			i++;
+			return((char *)&big[i]);
+		}
+		i++;
+		return(0);
+	}*/
+	
